684d7321e7c83e7440c86a13c1d7dd28bd6d8248
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-09-16 22:43:04 +0400

    Prisma Create method added.

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prima = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    // Create User
-    const user = await prisma.user.create({
-        data: {
-            name: "Shahjalal",
-            email: "shahjalal@gmail.com"
-        }
-    })
 
-    console.log(user)
+    // CREATE USER
+  const user = await prisma.user.create({
+    data: {
+      name: 'Shahjalal Hazari',
+      email: 'shahjalal.hazari@example.com',
+    },
+  });
+
+  console.log(user);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
