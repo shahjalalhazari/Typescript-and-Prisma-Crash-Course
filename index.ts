32e69d699b6d8469f15db93eda5fd85e702a6940
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-09-16 22:51:16 +0400

    Create method added for Article associate with user/author.

--- index.ts
+++ index.ts
@@ -12,14 +12,32 @@ async function main() {
         email: 'shahjalal.hazari@example.com',
         },
     });
-    
+
     // console.log(user);
     */
 
 
     // GET ALL USERS
+    /**
     const users = await prisma.user.findMany();
     console.log(users);
+     */
+
+    // CREATE ARTICLE ASSOCIATE WITH USER
+    const article = await prisma.article.create({
+        data: {
+            title: "My First Article",
+            content: "This is the content of my first article.",
+            author: {
+                connect: {
+                    id: 2
+                }
+            }
+        }
+    });
+
+    console.log(article);
+
 }
 
 main()
